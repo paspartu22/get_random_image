47aa443abcc9ac1fe789d20a0b4d28e4d8e1cc9e
Andrey Ovodov <a.ovodov@motorica.org>
2025-04-04 10:21:02 +0300

    changes

--- main.py
+++ main.py
@@ -1,29 +1,25 @@
 import requests
 import random
 import webbrowser
-from datetime import datetime
 
-    # Замените эти значения на свои    
-ACCESS_TOKEN = "put token here"  # Получить можно в Pinterest Developer Portal
+# Настройки
+ACCESS_TOKEN = "put key here"  # Ключ API
 MY_BOARD_ID = "636766903503568067"  # это моя маленькая борда на 3 случайных картинки
-BOARDS_ARRAY = ["636766903503568067"] #
+
+# Это список досок, из которых он будет забирать пины, ID можно получить запустив get_board_id.py
+BOARDS_ARRAY = [636766903503568067,
+                636766903504290013] #
+
+# = True если хочешь, чтобы пины не повторялись, = False, если пины могут повторяться
+NO_REPEATS = True 
+# NO_REPEATS = False
 
 
-def get_boards(access_token):
-    url = "https://api.pinterest.com/v5/boards"
-    headers = {"Authorization": f"Bearer {access_token}"}
-    params = {"fields": "id,name,url"}
-    response = requests.get(url, headers=headers, params=params)
-    return response.json().get("items", [])
-
-
+##########################
 
 def get_pins_from_boards(access_token, boards_id):
     pins_array = []
     for board_id in boards_id:
-        """
-        Получает случайный пин из указанной доски Pinterest
-        """
         url = f"https://api.pinterest.com/v5/boards/{board_id}/pins"
         headers = {
             "Authorization": f"Bearer {access_token}"
@@ -49,16 +45,14 @@ def get_pins_from_boards(access_token, boards_id):
     return pins_array
 
 def main():
-    # boards = get_boards(ACCESS_TOKEN)
-    # for board in boards:
-    #     print(f"ID: {board['id']}, Name: {board['name']}")
-    
     print("Подключаемся к Pinterest...")
     pins_array = get_pins_from_boards(ACCESS_TOKEN, BOARDS_ARRAY)
     
     if pins_array:
         while(1):
             random_pin = random.choice(pins_array)
+            if NO_REPEATS:
+                pins_array.remove(random_pin)
             print("\nСлучайная картинка из вашей папки:")
             print(f"Описание: {random_pin.get('note', 'Без описания')}")
             print(f"Дата создания: {random_pin.get('created_at', 'Неизвестно')}")
@@ -77,6 +71,9 @@ def main():
             else:
                 print("Не удалось получить URL изображения.")
             if (input("Нажми q чтобы выйти, любую другую клавишу, чтобы открыть еще одну картинку \n") == 'q'):
+                break
+            if (not pins_array):
+                print("Все пины закончились")
                 break
 
     else:
